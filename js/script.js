992000018600f50e61b6f9b8c459f74c25edcc19
Roomy6 <roomyk792@gmail.com>
2023-08-01 11:10:19 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,33 @@
-let inputString;
+const canvas    = document.getElementById("canvas");
+const context   = canvas.getContext("2d");
+const colorMenu = document.getElementById("colorMenu");
+const place     = document.getElementById("btnPlace");
 
-/* Get length of user input. */
+let currentColor = "#3e4a56";
 
-document.getElementById("button").onclick = function() {
-    inputString =   document.getElementById("inputText").value;
-    txtLbl = document.getElementById("textlabel").innerHTML = inputString.length;
+function drawCell(x, y) {
+    context.fillStyle = currentColor;
+    context.fillRect(x, y, 20, 20);
+}
 
-    console.log(inputString.length);
+function setColor(color) {
+    currentColor = color;
+}
+
+canvas.addEventListener("click", function (event) {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    //const cellX = Math.floor(x / 20) * 20;
+    //const cellY = Math.floor(y / 20) * 20;
+    drawCell(cellX, cellY);
+});
+
+place.onclick = function(event) {
+    const rect = canvas.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+    const cellX = Math.floor(x / 20) * 20;
+    const cellY = Math.floor(y / 20) * 20;
+    drawCell(cellX, cellY);
 }
